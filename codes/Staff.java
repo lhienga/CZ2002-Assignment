8eb903f63e09c04b5b72a86e8743993060e38e4e
lhienga <ngalh2002@gmail.com>
2021-10-31 22:31:28 +0800

    edit staff and staffManager

--- codes/Staff.java
+++ codes/Staff.java
@@ -6,44 +6,61 @@ public class Staff {
 	private String jobTitle;
 
 	/**
-	 * 
-	 * @param name
-	 * @param gender
+	 * Staff constructor 
+	 * @param name 
+	 * @param gender 'M' or 'F'
 	 * @param id
 	 * @param title
 	 */
 	public Staff(String name, char gender, int id, String title) {
 		// TODO - implement Staff.Staff
-		throw new UnsupportedOperationException();
+		this.name = name;
+		this.gender = gender;
+		this. staffID = id;
+		this.jobTitle = title;
 	}
-
+	/**
+	 * get staff name
+	 * @return staff name 
+	 */
 	public String getName() {
 		return this.name;
 	}
-
+	/**
+	 * get staff gender
+	 * @return staff gender 'M' or 'F'
+	 */
 	public char getGender() {
 		return this.gender;
 	}
 
 	/**
-	 * 
+	 * set staff name
 	 * @param name
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * get staff ID
+	 * @return staff id
+	 */
 	public int getID() {
 		// TODO - implement Staff.getID
-		throw new UnsupportedOperationException();
+		return this.staffID;
 	}
 
+	/**
+	 * get staff's job title
+	 * @return job title
+	 */
 	public String getJobTitle() {
 		return this.jobTitle;
 	}
 
 	/**
-	 * 
+	 * set staff's gender 'M' or 'F'
 	 * @param gender
 	 */
 	public void setGender(char gender) {
@@ -51,16 +68,16 @@ public class Staff {
 	}
 
 	/**
-	 * 
+	 * set staff ID
 	 * @param id
 	 */
 	public void setID(int id) {
 		// TODO - implement Staff.setID
-		throw new UnsupportedOperationException();
+		this.staffID = id;
 	}
 
 	/**
-	 * 
+	 * set staff job title
 	 * @param title
 	 */
 	public void setJobTitle(String title) {
--- codes/StaffManager.java
+++ codes/StaffManager.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+
 public class StaffManager {
 
-	private ArrayList<Staff> staff;
-
+	private ArrayList<Staff> staffs;
+	/**
+	 * construct staff manager
+	 */
 	public StaffManager() {
 		// TODO - implement StaffManager.StaffManager
-		throw new UnsupportedOperationException();
+		staffs = new ArrayList<Staff>();
 	}
 
 	/**
-	 * 
+	 * add new staff
 	 * @param name
 	 * @param gender
 	 * @param id
@@ -17,48 +20,82 @@ public class StaffManager {
 	 */
 	public void addStaff(String name, char gender, int id, String title) {
 		// TODO - implement StaffManager.addStaff
-		throw new UnsupportedOperationException();
+		Staff curStaff = this.getStaffByID(id);
+		if (curStaff!=null){
+			System.out.println("This staff ID existed!");
+			return;
+		}
+		Staff newStaff = new Staff(name, gender, id, title);
+		staffs.add(newStaff);
+		System.out.println("New staff added!");
 	}
 
 	/**
-	 * 
+	 * remove staff
 	 * @param id
 	 */
 	public void removeStaff(int id) {
 		// TODO - implement StaffManager.removeStaff
-		throw new UnsupportedOperationException();
+		Staff curStaff = this.getStaffByID(id);
+		if (curStaff==null){
+			System.out.println("This Staff ID does not exist!");
+			return;
+		}
+		staffs.remove(curStaff);
+		System.out.println("Staff removed!");
 	}
 
 	/**
-	 * 
+	 * find staff by id
 	 * @param id
+	 * @return staff or null
 	 */
 	public Staff getStaffByID(int id) {
 		// TODO - implement StaffManager.getStaffByID
-		throw new UnsupportedOperationException();
+		for (int i =0; i<staffs.size(); i++){
+			if (staffs.get(i).getID()==id){
+				return staffs.get(i);
+			}
+		}
+		return null;
 	}
-
+	/**
+	 * get all staff
+	 * @return staffs
+	 */
 	public ArrayList<Staff> getAllStaff() {
 		// TODO - implement StaffManager.getAllStaff
-		throw new UnsupportedOperationException();
+		return this.staffs;
 	}
 
 	/**
-	 * 
+	 * print staff by ID
 	 * @param id
 	 */
 	public void printStaffByID(int id) {
 		// TODO - implement StaffManager.printStaffByID
-		throw new UnsupportedOperationException();
+		Staff curStaff = getStaffByID(id);
+		if (curStaff==null){
+			System.out.println("Staff ID does not exist!");
+			return;
+		}
+		System.out.println("Staff ID: "+curStaff.getID()+"\n"+
+						   "Staff name: "+curStaff.getName()+"\n"+
+						   "Gender: "+curStaff.getGender()+"\n"+						   
+						   "Title: "+curStaff.getJobTitle());
 	}
-
+	/**
+	 * print all staff
+	 */
 	public void printAllStaffs() {
 		// TODO - implement StaffManager.printAllStaffs
-		throw new UnsupportedOperationException();
+		for (int i = 0; i<staffs.size(); i++){
+			this.printStaffByID(staffs.get(i).getID());
+		}
 	}
 
 	/**
-	 * 
+	 * update staff information
 	 * @param id
 	 */
 	public void updateStaffInfo(int id) {
