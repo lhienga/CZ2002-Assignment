deb7bc74ed2b53e536c0fce6493667cafb354817
EliseTZ2910 <79182585+EliseTZ2910@users.noreply.github.com>
2021-11-04 17:57:44 +0800

    Update Order.java

--- codes/Order.java
+++ codes/Order.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList; // import the ArrayList class
-import java.util.Date;
+import java.util.Calendar;
+
 public class Order {
 
 	private int tableNum;
-	private Date timeStamp;
+	private Calendar timeStampOfSettledPayment;
 	private int staffID;
 	private String staffName;
 	private ArrayList<MenuItem> menuItem;
@@ -69,7 +70,7 @@ public class Order {
 	 */
 	public double getItemPrice(MenuItem item, Menu menu) {
 		// TODO - implement Order.getItemPrice
-		ArrayList<MenuItem> menuItems = (ArrayList<MenuItem>)menu.getMenuItems().clone();
+		ArrayList<MenuItem> menuItems = menu.getMenuItems();
 		for (int i = 0; i< menu.getMenuSize(1); i++) {
 			MenuItem currItem = menuItems.get(i);
 			if (item == currItem) {
@@ -103,14 +104,22 @@ public class Order {
 	}
 
 	/**
-	 * get order time
-	 * @return time and date the order is made
+	 * get time of settled payment
+	 * @return time and date the order is paid
 	 */
-	public Date getOrderTime() {
+	public Calendar getPaymentTime() {
 		// TODO - implement Order.getOrderTime
-		return this.timeStamp;
+		return this.timeStampOfSettledPayment;
 	}
 
+	/**
+	 * set  time of settled payment
+	 * @param settledPaymenttime
+	 * @return
+	 */
+	public void setPaymentTime(Calendar settledPaymenttime) {
+		this.timeStampOfSettledPayment = settledPaymenttime;
+	}
 	/**
 	 * get staff's ID
 	 * @return the ID of staff taking the order
@@ -135,6 +144,33 @@ public class Order {
 		// TODO - implement Order.getOrder
 		return this.menuItem;
 	}
+	
+	/**
+	 * print order
+	 */
+	public void printOrder() {
+		int j=1;
+		int k=1;
+		for (MenuItem item : menuItem) {
+			j = 1;
+			if(item instanceof AlaCarte) {
+				System.out.printf("%d. %-20s %s\nPrice: $%.2f \nDescription: %s\n\n",k,
+						item.getName(), "(AlaCarte - "+item.getType()+")", item.getPrice(), item.getDesc());
+			} else if(item instanceof PromotionPackage){
+				System.out.printf("%d. %-20s %s\n",k,
+						item.getName(), "(Promotion Set)", item.getPrice(), item.getDesc());
+				for (Food food : item.getPackage()) {
+					System.out.printf("%d: %s: %s\n", j,
+							food.getType(), food.getName());
+					j++;
+				}
+					
+				System.out.printf("Price: $%.2f\nDescription:%s\n\n",
+						item.getPrice(), item.getDesc());
+			}
+			k++;
 
+		}
+	}
 
 }
