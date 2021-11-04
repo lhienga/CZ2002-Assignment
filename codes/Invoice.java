f7d8f9c1ae7ad46edc432f7662c5d07dde3d13c4
EliseTZ2910 <79182585+EliseTZ2910@users.noreply.github.com>
2021-11-04 17:53:24 +0800

    Update Invoice.java

--- codes/Invoice.java
+++ codes/Invoice.java
@@ -3,16 +3,27 @@ import java.util.Calendar;
 public class Invoice {
 	private Order paidOrder;
 	private int invoiceID;
+	private double subtotal;
 	private double totalPrice;
+	private double gst;
+	private double serviceCharge;
+	private double membershipDiscount=0;
 	private Calendar dateCreated;
+	private boolean isMember;
 	
-	public Invoice(Order order, double totalPrice){
+	public Invoice(Order order,boolean isMember,double subtotal) {
 		this.paidOrder = order;
+		this.dateCreated = order.getPaymentTime();
 		this.invoiceID = Calendar.getInstance().hashCode();
-		this.totalPrice = totalPrice;
-		this.dateCreated = Calendar.getInstance();
+		this.subtotal = totalPrice;
+		this.gst = 0.07*totalPrice;
+		this.serviceCharge = 0.1*totalPrice;
+		if (isMember) {
+			membershipDiscount = 0.1*totalPrice;
+		}
+		this.totalPrice = subtotal + gst + serviceCharge +membershipDiscount;
+		
 	}
-	
 	public Calendar getDateCreated() {
 		return dateCreated;
 	}
@@ -27,8 +38,13 @@ public class Invoice {
 	
 	//printInvoice use ORDERMANAGER ONE!!!
 	public void printInvoice(){
-		System.out.println("Date & Time: " + this.paidOrder.getOrderTime());
+		System.out.println("Date & Time: " + this.paidOrder.getPaymentTime());
 		System.out.println("Invoice Number: " + this.invoiceID);
-		System.out.println("Total: " + totalPrice);
+		paidOrder.printOrder();
+		System.out.println("Subtotal: " + this.paidOrder);
+		System.out.println("Membership Discount: " + membershipDiscount);
+		System.out.println("Service Charge : " + serviceCharge);
+		System.out.println("Total Payable: " + totalPrice);
+
 	}
 }
