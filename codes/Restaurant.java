bdcc5673e981eb7765975b1bcf86628ce10badfa
EliseTZ2910 <79182585+EliseTZ2910@users.noreply.github.com>
2021-11-06 04:03:09 +0800

    Update Restaurant.java

--- codes/Restaurant.java
+++ codes/Restaurant.java
@@ -13,9 +13,6 @@ import java.util.ArrayList;
 /**
  * Static restaurant database class keeping track of
  * objects in the restaurant
- * @author soh jun jie
- * @version 1.0
- * @since 2016-11-09
  */
 public class Restaurant {
 	
@@ -135,10 +132,10 @@ public class Restaurant {
 			for (int i=2;i<=10;i+=2) {
 				for (int k=1;k<=4;k++) {
 					if (k%2==0) {
-						tables.add(new Table(j,i,Table.STATUS.AVAILABLE,true));
+						tables.add(new Table(j,i,true));
 					}
 					else {
-						tables.add(new Table(j,i,Table.STATUS.AVAILABLE,false));
+						tables.add(new Table(j,i,false));
 					}
 					
 					j++;
@@ -170,15 +167,15 @@ public class Restaurant {
 			
 			ArrayList<Food>food = new ArrayList<Food>();
 			
-			food.add(new Food("Miniature crab cakes", "Cakes that taste like crab",2, Food.TYPE.APPETIZER));
-			food.add(new Food("Mac and Cheese", "Chessy macaroni", 1.50, Food.TYPE.APPETIZER));
-			food.add(new Food("Caprese salad", "Extremely healthy but yummy", 1.80, Food.TYPE.APPETIZER));
-			food.add(new Food("Chicken Soup", "Old Fashion and simple soup", 1.00, Food.TYPE.APPETIZER));
+			food.add(new Food("Miniature crab cakes", "Cakes that taste like crab",4, Food.TYPE.APPETIZER));
+			food.add(new Food("Mac and Cheese", "Chessy macaroni", 2.50, Food.TYPE.APPETIZER));
+			food.add(new Food("Caprese salad", "Extremely healthy but yummy", 2.80, Food.TYPE.APPETIZER));
+			food.add(new Food("Chicken Soup", "Old Fashion and simple soup", 2.00, Food.TYPE.APPETIZER));
 			
-			food.add(new Food("Beef Burger", "Freshly grilled beef", 4, Food.TYPE.MAINCOURSE));
-			food.add(new Food("Classic Cheese Burger", "The usual", 2, Food.TYPE.MAINCOURSE));
-			food.add(new Food("Double-up Cheese Burger", "Double the trouble, double the satisfaction", 3.50, Food.TYPE.MAINCOURSE));
-			food.add(new Food("Fish Burger", "Deep fried fish with tartar sauce", 2.50, Food.TYPE.MAINCOURSE));
+			food.add(new Food("Beef Burger", "Freshly grilled beef", 5, Food.TYPE.MAINCOURSE));
+			food.add(new Food("Classic Cheese Burger", "The usual", 3, Food.TYPE.MAINCOURSE));
+			food.add(new Food("Double-up Cheese Burger", "Double the trouble, double the satisfaction", 5.50, Food.TYPE.MAINCOURSE));
+			food.add(new Food("Fish Burger", "Deep fried fish with tartar sauce", 3.50, Food.TYPE.MAINCOURSE));
 			food.add(new Food("Grilled Chicken Burger", "Freshly grilled chicken", 3, Food.TYPE.MAINCOURSE));
 
 			food.add(new Food("Coke", "Just Coke",1.70, Food.TYPE.DRINK));
@@ -187,9 +184,9 @@ public class Restaurant {
 			food.add(new Food("Water", "H2O", 1.00, Food.TYPE.DRINK));
 
 			food.add(new Food("Ice cream cone", "Two scoops of ice cream on a cone", 2.50, Food.TYPE.DESSERT));
-			food.add(new Food("Chocolate Brownie", "Mouth watering chocolate", 2.50, Food.TYPE.DESSERT));
-			food.add(new Food("Banana Split", "Banana with ice cream", 4.50, Food.TYPE.DESSERT));
-			food.add(new Food("Cheese Cake", "Just Cheese", 3.50, Food.TYPE.DESSERT));
+			food.add(new Food("Chocolate Brownie", "Mouth watering chocolate", 4.50, Food.TYPE.DESSERT));
+			food.add(new Food("Banana Split", "Banana with ice cream", 5.50, Food.TYPE.DESSERT));
+			food.add(new Food("Cheese Cake", "Just Cheese", 4.50, Food.TYPE.DESSERT));
 
 			for(int i=0; i<food.size(); i++)
 				menu.add(new AlaCarte(food.get(i)));
@@ -207,9 +204,17 @@ public class Restaurant {
 			set2.add(food.get(11));
 			set2.add(food.get(14));
 			set2.add(food.get(15));
+			
+			ArrayList<Food>set3 = new ArrayList<Food>();
+			set3.add(food.get(4));
+			set3.add(food.get(5));
+			set3.add(food.get(6));
+			set3.add(food.get(7));
+			set3.add(food.get(8));
 
-			menu.add(new PromotionPackage(set1, "Chicken Burger set","Easy Combo",7.0 ));
-			menu.add(new PromotionPackage(set2, "Cheese buddy set", "Easy Combo",14.0));
+			menu.add(new PromotionPackage(set1, "Chicken Burger set","Easy Combo",9.00 ));
+			menu.add(new PromotionPackage(set2, "Cheese buddy set", "Cheese lovers",18.0));
+			menu.add(new PromotionPackage(set3, "Burger Part set", "Many burgers in one meal",15.0));
             Restaurant.foodMenu = menu;
 	}
 	
